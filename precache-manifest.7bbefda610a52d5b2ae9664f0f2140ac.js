75c5866e93cfc3ae1abb0d5b71f1cf28bb7730cf
yusrilarzaqi <arzaqiyusril@gmail.com>
2022-07-01 22:26:21 +0700

    add my new portfolio

---
+++ precache-manifest.7bbefda610a52d5b2ae9664f0f2140ac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6dc443a557030fef250",
+    "url": "/static/css/main.e8b72155.chunk.css"
+  },
+  {
+    "revision": "d6dc443a557030fef250",
+    "url": "/static/js/main.7c259efe.chunk.js"
+  },
+  {
+    "revision": "7e45d5088be90e613b95",
+    "url": "/static/js/runtime~main.bcf80d21.js"
+  },
+  {
+    "revision": "b41b79e38109ead0559f",
+    "url": "/static/js/2.fbd5a048.chunk.js"
+  },
+  {
+    "revision": "a9807912173ef280ea3f",
+    "url": "/static/js/3.9f01db21.chunk.js"
+  },
+  {
+    "revision": "917915cd9da7ca42e6fd8f79846f8464",
+    "url": "/static/media/me-2.917915cd.png"
+  },
+  {
+    "revision": "bbd54da1d3fcbb4cf9a8ae6f5425ace7",
+    "url": "/static/media/linux.bbd54da1.png"
+  },
+  {
+    "revision": "5fd96b8e103fbb58467aed7cd54e706a",
+    "url": "/static/media/project-1.5fd96b8e.png"
+  },
+  {
+    "revision": "f9cd57e3e131233b76ed3255dffefd81",
+    "url": "/static/media/project-2.f9cd57e3.png"
+  },
+  {
+    "revision": "18101a305887c3206e7e2628dde0f052",
+    "url": "/static/media/project-3.18101a30.png"
+  },
+  {
+    "revision": "5f3082a3daedb3b3881753c418850be9",
+    "url": "/static/media/project-4.5f3082a3.png"
+  },
+  {
+    "revision": "909c6915fd579ed8caf5b3ecb88d7f12",
+    "url": "/static/media/project-5.909c6915.png"
+  },
+  {
+    "revision": "4f08cbb3e9997e035698872c72f3c12c",
+    "url": "/static/media/project-6.4f08cbb3.png"
+  },
+  {
+    "revision": "077a669deff84a5c992677689442b428",
+    "url": "/index.html"
+  }
+];
