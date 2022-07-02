28baf65fca8fbe32b31bfe07377414651c589c43
yusrilarzaqi <arzaqiyusril@gmail.com>
2022-07-02 11:08:06 +0700

    Updates

---
+++ precache-manifest.5428587e259c9c0b0bc02096606563ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b8b8370f4f10a8ce6b",
+    "url": "/Portfolio/static/css/main.e8b72155.chunk.css"
+  },
+  {
+    "revision": "f3b8b8370f4f10a8ce6b",
+    "url": "/Portfolio/static/js/main.30ca28fd.chunk.js"
+  },
+  {
+    "revision": "41ddaae545138189d6e7",
+    "url": "/Portfolio/static/js/runtime~main.8e5d9f8e.js"
+  },
+  {
+    "revision": "e02ac13360612cdbfdf5",
+    "url": "/Portfolio/static/js/2.b0ac0311.chunk.js"
+  },
+  {
+    "revision": "e2aa523fcf964839130f",
+    "url": "/Portfolio/static/js/3.ec459d4c.chunk.js"
+  },
+  {
+    "revision": "917915cd9da7ca42e6fd8f79846f8464",
+    "url": "/Portfolio/static/media/me-2.917915cd.png"
+  },
+  {
+    "revision": "bbd54da1d3fcbb4cf9a8ae6f5425ace7",
+    "url": "/Portfolio/static/media/linux.bbd54da1.png"
+  },
+  {
+    "revision": "5fd96b8e103fbb58467aed7cd54e706a",
+    "url": "/Portfolio/static/media/project-1.5fd96b8e.png"
+  },
+  {
+    "revision": "f9cd57e3e131233b76ed3255dffefd81",
+    "url": "/Portfolio/static/media/project-2.f9cd57e3.png"
+  },
+  {
+    "revision": "18101a305887c3206e7e2628dde0f052",
+    "url": "/Portfolio/static/media/project-3.18101a30.png"
+  },
+  {
+    "revision": "5f3082a3daedb3b3881753c418850be9",
+    "url": "/Portfolio/static/media/project-4.5f3082a3.png"
+  },
+  {
+    "revision": "909c6915fd579ed8caf5b3ecb88d7f12",
+    "url": "/Portfolio/static/media/project-5.909c6915.png"
+  },
+  {
+    "revision": "4f08cbb3e9997e035698872c72f3c12c",
+    "url": "/Portfolio/static/media/project-6.4f08cbb3.png"
+  },
+  {
+    "revision": "4c05a5ecbadb44ce5ebd6e943cc7ef2d",
+    "url": "/Portfolio/index.html"
+  }
+];
